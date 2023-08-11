f3c817858da981e12c2145589c58d2e3f39ed78a
Anees Haroon <aharoon1007@gmail.com>
2023-08-11 21:01:15 +0100

    Update Card styling

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -2,9 +2,11 @@ import { cookies } from "next/headers";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
+import Card from "./Card";
+
 const Events = async () => {
   const supabase: SupabaseClient = createServerComponentClient({ cookies });
-    const {data: events} = await supabase.from("events").select();
+    const {data: events} = await supabase.from("events").select().limit(4);
 
   return (
     <>
@@ -12,11 +14,9 @@ const Events = async () => {
         See a selection of our events below:
       </h1>
       <div className="columns m-4">
-        <ul>
           {events?.map((event: DBEvent) => (
-            <li key={event.id}>{event.name}</li>
+            <Card key = {event.id} {...event}/>
           ))}
-        </ul>
       </div>
       <h1 className="title is-3 is-size-4-mobile has-text-centered has-text-weight-semibold purple p-6">
         You can see all of our events <a className="gradient-text">here</a>
