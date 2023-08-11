58fabd25de040bad05a4938daf90faf3facc5f4b
Anees Haroon <aharoon1007@gmail.com>
2023-08-11 20:48:57 +0100

    Create Card structure

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Card = (props: DBEvent) => {
+  return (
+    <div id="events">
+      <div className="columns m-4">
+        <div className="is-flex is-flex-direction-column is-justify-content-space-between column event m-2 p-6 purple-box">
+          <div>
+            <h2 className="mt-2 subtitle is-5 is-size-6-mobile is-underlined has-text-white">
+              {props.location}
+            </h2>
+            <h1 className="title is-4 is-size-5-mobile has-text-white">
+              {props.name}
+            </h1>
+            <h2 className="mt-2 subtitle is-5 is-size-6-mobile has-text-white">
+              {props.duration}
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
