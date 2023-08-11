bf0c1a01b13a87ecc15eacf03352abd01cfe35bf
Anees Haroon <aharoon1007@gmail.com>
2023-08-11 21:22:31 +0100

    Change Header component prop type declaration

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,11 @@
-const Header = (title: string) => {
+type Props = {
+    title: string
+}
+
+const Header = (props: Props) => {
   return (
     <h1 className ="title is-1 is-size-2-mobile has-text-weight-semibold has-text-white p-6 m-6">
-        {title}
+        {props.title}
     </h1>
   )
 }
