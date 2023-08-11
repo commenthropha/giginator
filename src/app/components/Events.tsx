752776a64e6204b811cdaa8ae48b9ee6b40a062b
Anees Haroon <aharoon1007@gmail.com>
2023-08-11 20:13:04 +0100

    Add database read functionality

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -1,15 +1,28 @@
-const Events = () => {
-    return (
-      <>
-        <h1 className="title is-1 is-size-2-mobile has-text-centered has-text-weight-semibold gradient-text p-6">
-          See a selection of our events below:
-        </h1>
-        <div className="columns m-4"></div>
-        <h1 className="title is-3 is-size-4-mobile has-text-centered has-text-weight-semibold purple p-6">
-          You can see all of our events <a className="gradient-text">here</a>
-        </h1>
-      </>
-    );
-  };
+import { cookies } from "next/headers";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export default Events
+const Events = async () => {
+  const supabase: SupabaseClient = createServerComponentClient({ cookies });
+    const {data: events} = await supabase.from("events").select();
+
+  return (
+    <>
+      <h1 className="title is-1 is-size-2-mobile has-text-centered has-text-weight-semibold gradient-text p-6">
+        See a selection of our events below:
+      </h1>
+      <div className="columns m-4">
+        <ul>
+          {events?.map((event) => (
+            <li key={event.id}>{event.name}</li>
+          ))}
+        </ul>
+      </div>
+      <h1 className="title is-3 is-size-4-mobile has-text-centered has-text-weight-semibold purple p-6">
+        You can see all of our events <a className="gradient-text">here</a>
+      </h1>
+    </>
+  );
+};
+
+export default Events;
