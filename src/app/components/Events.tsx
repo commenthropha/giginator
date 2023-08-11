9aaf5ea0056ae45c8754066b3e0c551e3b256506
Anees Haroon <aharoon1007@gmail.com>
2023-08-11 15:41:51 +0100

    Migrate React component code to Events.tsx

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
-type Props = {}
-
-const Events = (props: Props) => {
-  return (
-    <div>Events</div>
-  )
-}
+const Events = () => {
+    return (
+      <>
+        <h1 className="title is-1 is-size-2-mobile has-text-centered has-text-weight-semibold gradient-text p-6">
+          See a selection of our events below:
+        </h1>
+        <div className="columns m-4"></div>
+        <h1 className="title is-3 is-size-4-mobile has-text-centered has-text-weight-semibold purple p-6">
+          You can see all of our events <a className="gradient-text">here</a>
+        </h1>
+      </>
+    );
+  };
 
 export default Events
