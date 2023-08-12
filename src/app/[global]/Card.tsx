c992a88005b51f5116737d5c7134b6015ba0ff8b
Anees Haroon <aharoon1007@gmail.com>
2023-08-12 02:19:08 +0100

    Change Card parameter name from props to event

--- src/app/[global]/Card.tsx
+++ src/app/[global]/Card.tsx
@@ -1,15 +1,15 @@
-const Card = (props: DBEvent) => {
+const Card = (event: DBEvent) => {
   return (
     <div className="is-flex is-flex-direction-column is-justify-content-space-between column event m-2 p-6 event">
       <div>
         <h2 className="mt-2 subtitle is-5 is-size-6-mobile is-underlined has-text-white">
-          {props.location}
+          {event.location}
         </h2>
         <h1 className="title is-4 is-size-5-mobile has-text-white">
-          {props.name}
+          {event.name}
         </h1>
         <h2 className="mt-2 subtitle is-5 is-size-6-mobile has-text-white">
-          {props.duration}
+          {event.duration}
         </h2>
       </div>
     </div>
