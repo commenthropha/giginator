b0a968e39733dca444bcdfeee9af6900091157fd
Anees Haroon <aharoon1007@gmail.com>
2023-08-13 14:00:23 +0100

    Edit Footing padding, Sign Up form styling

--- src/app/sign-in/components/Form.tsx
+++ src/app/sign-in/components/Form.tsx
@@ -9,41 +9,43 @@ const Form = () => {
             Sign In
           </h1>
           <form id="main" method="post">
-            <div className="field pt-2">
-              <label htmlFor="email" className="label has-text-white">
-                Email
-              </label>
-              <div className="control">
-                <input
-                  name="email"
-                  className="input"
-                  type="text"
-                  placeholder="Enter Your Email"
-                  value=""
-                />
-              </div>
-            </div>
-            <div className="field">
-              <label htmlFor="password" className="label has-text-white">
-                Password
-              </label>
-              <div className="control">
-                <input
-                  name="password"
-                  className="input"
-                  type="password"
-                  placeholder="Enter Your Password"
-                  value=""
-                />
-              </div>
-            </div>
-            <div className="field my-5">
-              <label className="label has-text-white">
-                {`Don't have an account yet?`} <br />
-                <Link className="has-text-warning" href="/sign-up">
-                  Sign Up Here
-                </Link>
-              </label>
+            <div className={`my-6 ${styles.customPadding}`}>
+                <div className="field pt-2">
+                  <label htmlFor="email" className="label has-text-white">
+                    Email
+                  </label>
+                  <div className="control">
+                    <input
+                      name="email"
+                      className="input"
+                      type="text"
+                      placeholder="Enter Your Email"
+                      value=""
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label htmlFor="password" className="label has-text-white">
+                    Password
+                  </label>
+                  <div className="control">
+                    <input
+                      name="password"
+                      className="input"
+                      type="password"
+                      placeholder="Enter Your Password"
+                      value=""
+                    />
+                  </div>
+                </div>
+                <div className="field my-5">
+                  <label className="label has-text-white">
+                    {`Don't have an account yet?`} <br />
+                    <Link className="has-text-warning" href="/sign-up">
+                      Sign Up Here
+                    </Link>
+                  </label>
+                </div>
             </div>
             <div className="center has-text-centered mx-2">
               <button className="mx-1 button is-medium is-light">
