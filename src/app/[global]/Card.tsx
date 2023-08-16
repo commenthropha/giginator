a7715a4a0efbf9f518711b2599e74d67cd93210e
Anees Haroon <aharoon1007@gmail.com>
2023-08-16 12:32:14 +0100

    Update Card component div structure

--- src/app/[global]/Card.tsx
+++ src/app/[global]/Card.tsx
@@ -1,17 +1,17 @@
 const Card = (event: DBEvent) => {
   return (
     <div className="is-flex is-flex-direction-column is-justify-content-space-between column event m-2 p-6 event">
-      <div>
+      <div className="mb-6">
         <h2 className="mt-2 subtitle is-5 is-size-6-mobile is-underlined has-text-white">
           {event.location}
         </h2>
         <h1 className="title is-4 is-size-5-mobile has-text-white">
           {event.name}
         </h1>
-        <h2 className="mt-2 subtitle is-5 is-size-6-mobile has-text-white">
-          {event.duration}
-        </h2>
       </div>
+      <h2 className="mt-2 subtitle is-5 is-size-6-mobile has-text-white">
+        {event.duration}
+      </h2>
     </div>
   );
 };
