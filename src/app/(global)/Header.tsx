efa62deb3e583c09d72734205b43260aa26b9cb8
Anees Haroon <aharoon1007@gmail.com>
2023-08-17 12:27:08 +0100

    Add comments to global Header component

--- src/app/(global)/Header.tsx
+++ src/app/(global)/Header.tsx
@@ -1,15 +1,17 @@
+// Create a type Alias for the Component props
 type Props = {
-    title: string
-}
+  // The title for the Header to display
+  title: string;
+};
 
 const Header = (props: Props) => {
   return (
     <div className="gradient">
-        <h1 className ="title is-1 is-size-2-mobile has-text-weight-semibold has-text-white p-12">
-            {props.title}
-        </h1>
+      <h1 className="title is-1 is-size-2-mobile has-text-weight-semibold has-text-white p-12">
+        {props.title}
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
