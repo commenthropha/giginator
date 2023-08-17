c8ea924ed546c4f89b823f2ced39d6a01831435a
Anees Haroon <aharoon1007@gmail.com>
2023-08-17 20:34:42 +0100

    Refactor organisation/ table as a separate component

---
+++ src/app/organisation/components/Table.tsx
@@ -0,0 +1,43 @@
+import { getOrganisedEvents } from "@/app/(queries)";
+import React from "react";
+
+const Table = async ({events}:{events: DBEvent[] | null}) => {
+  return (
+    <div className="table-container">
+      <table className="table is-fullwidth">
+        <thead className="gradient">
+          <tr className="is-size-5">
+            <th className="has-text-white">Name</th>
+            <th className="has-text-white">Location</th>
+            <th className="has-text-white">Date</th>
+            <th className="has-text-white">Time</th>
+            <th className="has-text-white">Duration</th>
+            <th className="has-text-white">Attendees</th>
+            <th className="has-text-white">Capacity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {events ? (
+            events.map((event: DBEvent) => (
+              <tr>
+                <td className="has-text-weight-bold">
+                  <a className="has-text-dark">{event.name}</a>
+                </td>
+                <td>{event.location}</td>
+                <td>{event.date}</td>
+                <td>{event.time}</td>
+                <td>{event.duration}</td>
+                <td>{event.tickets}</td>
+                <td>{event.capacity}</td>
+              </tr>
+            ))
+          ) : (
+            <p>Nothing here</p>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
--- src/app/organisation/page.tsx
+++ src/app/organisation/page.tsx
@@ -1,9 +1,34 @@
-import { Header } from "../(global)"
+import { redirect } from "next/navigation";
+import { Header } from "../(global)";
+import { Table } from "./components";
+import { getOrganisedEvents, getSession, isUserOrganiser } from "../(queries)";
 
-const Organisation = () => {
+const Organisation = async () => {
+  const session = await getSession();
+  const isOrganiser = await isUserOrganiser();
+  const organisedEvents: DBEvent[] | null = isOrganiser
+    ? await getOrganisedEvents()
+    : null;
+
+  if (!session) {
+    redirect("/");
+  }
+
+  if (!isOrganiser) {
+    redirect("/404");
+  }
+
   return (
-    <Header title = "Organisation" />
-  )
-}
+    <div>
+      <Header title="Organisation" />
+      <div className="overview">
+        <h2 className="gradient-text title is-2 is-size-3-mobile has-text-weight-semibold m-6">
+          Overview Of Your Events
+        </h2>
+        <Table events={organisedEvents}/>
+      </div>
+    </div>
+  );
+};
 
-export default Organisation
+export default Organisation;
