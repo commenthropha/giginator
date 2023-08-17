80b67618ca3f584f484ed4251eeeb0143d14c9e9
Anees Haroon <aharoon1007@gmail.com>
2023-08-17 20:47:30 +0100

    Update dashboard UserEvents component

--- src/app/dashboard/components/UserEvents.tsx
+++ src/app/dashboard/components/UserEvents.tsx
@@ -2,7 +2,27 @@ import { Card } from "@/app/(global)";
 
 const UserEvents = ({events}: {events: DBEvent[] | null}) => {
   return (
-    <div>UserEvents</div>
+    <div id="booked-events">
+        <h2 className="gradient-text title is-2 is-size-3-mobile has-text-weight-semibold m-6">
+          Your Events
+        </h2>
+
+        {/* If there are no events that the user has booked, 
+            let them know; otherwise return all the events that 
+            they have organised as card components*/}
+
+        {events === null ? (
+          <p className="mx-5 px-3 is-size-4 is-size-5-mobile">
+            {`You haven't booked any events yet.`}
+          </p>
+        ) : (
+          <div className="is-desktop columns m-4">
+            {events.map((event: DBEvent) => (
+              <Card key={event.id} event={event} text={true} />
+            ))}
+          </div>
+        )}
+      </div>
   )
 }
 
