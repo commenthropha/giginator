d715b1c74f499c5ee2dce470c173693d71f5d662
Anees Haroon <aharoon1007@gmail.com>
2023-08-17 14:17:16 +0100

    Add comments to query isUserOrganiser

--- src/app/(queries)/isUserOrganiser.ts
+++ src/app/(queries)/isUserOrganiser.ts
@@ -1,13 +1,21 @@
-import { SupabaseClient, createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import {
+  SupabaseClient,
+  createServerComponentClient,
+} from "@supabase/auth-helpers-nextjs";
 import { getSession } from ".";
 import { cookies } from "next/headers";
 
 const isUserOrganiser = async () => {
+  // Initialise Supabase client
   const cookieStore = cookies();
-  const supabase: SupabaseClient = createServerComponentClient({ cookies: () => cookieStore });
-  
+  const supabase: SupabaseClient = createServerComponentClient({
+    cookies: () => cookieStore,
+  });
+
+  // Retrieve session data
   const session = await getSession();
 
+  // Query the database and return the user organiser status
   return await supabase
     .from("profiles")
     .select("organiser")
