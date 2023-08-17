4b2cae9182788ce4d4591fca6323ca021f375f48
Anees Haroon <aharoon1007@gmail.com>
2023-08-17 14:01:35 +0100

    Add comments to query getOrganisedEvents

--- src/app/(queries)/getOrganisedEvents.ts
+++ src/app/(queries)/getOrganisedEvents.ts
@@ -3,21 +3,28 @@ import { cookies } from "next/headers";
 import { getSession, isUserOrganiser } from ".";
 
 const getOrganisedEvents = async () => {
+  // Initialise Supabase client
   const cookieStore = cookies();
   const supabase: SupabaseClient = createServerComponentClient({ cookies: () => cookieStore });
 
+  // Retrieve session data
   const session = await getSession();
+
+  // Check if a current user is an organiser
   const isOrganiser = await isUserOrganiser();
 
   if (isOrganiser) {
+    // Get the organised event IDs from the association table
     const { data: organisedEvents } = await supabase
       .from("event_organisers")
       .select("organised_event_id")
       .eq("organiser_id", session?.user.id);
 
     if (organisedEvents) {
-      const eventIds = organisedEvents.map((event) => event.organised_event_id);
+      // Extract the event IDs
+      const eventIds = organisedEvents.map((e) => e.organised_event_id);
           
+      // Get the events associated with the IDs from the events table
       const { data: events } = await supabase
         .from("events")
         .select()
