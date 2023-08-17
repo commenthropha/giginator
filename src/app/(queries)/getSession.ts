c1479d59b8a4880664cbecfbc85361ae5413f3fb
Anees Haroon <aharoon1007@gmail.com>
2023-08-17 14:16:18 +0100

    Add comments to query getSession

--- src/app/(queries)/getSession.ts
+++ src/app/(queries)/getSession.ts
@@ -1,14 +1,22 @@
-import { SupabaseClient, createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import {
+  SupabaseClient,
+  createServerComponentClient,
+} from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-const getSession = async() => {
+const getSession = async () => {
+  // Initialise Supabase client
   const cookieStore = cookies();
-  const supabase: SupabaseClient = createServerComponentClient({ cookies: () => cookieStore });
-    const {
-      data: { session },
-    } = await supabase.auth.getSession();
+  const supabase: SupabaseClient = createServerComponentClient({
+    cookies: () => cookieStore,
+  });
 
-    return session;
-}
+  // Call the Supabase auth client to check if there is a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return session;
+};
 
 export default getSession;
