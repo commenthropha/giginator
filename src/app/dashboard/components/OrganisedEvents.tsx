22cc1b39051331349c9c7151b41484dfa366c8ee
Anees Haroon <aharoon1007@gmail.com>
2023-08-17 20:42:47 +0100

    Refactor dashboard organised events into separate component file

---
+++ src/app/dashboard/components/OrganisedEvents.tsx
@@ -0,0 +1,43 @@
+import { Card } from "@/app/(global)";
+
+const OrganisedEvents = ({
+  isOrganiser,
+  events,
+}: {
+  isOrganiser: boolean;
+  events: DBEvent[] | null;
+}) => {
+  return (
+    /* Inline ternary operator checks if the user is an organiser 
+    to determine if this needs to be rendered at all. */
+
+    isOrganiser ? (
+      <div id="organised-events">
+        <h2 className="gradient-text title is-2 is-size-3-mobile has-text-weight-semibold m-6">
+          Organised Events
+        </h2>
+
+        {/* If there are no events that the user has organised, 
+              let them know; otherwise return all the events that 
+              they have organised as card components*/}
+
+        {events === null ? (
+          <p className="mx-5 px-3 is-size-4 is-size-5-mobile">
+            {`You haven't organised any events yet.`}
+          </p>
+        ) : (
+          <div className="columns is-desktop m-4">
+            {events.map((event: DBEvent) => (
+              <Card key={event.id} event={event} text={true} />
+            ))}
+          </div>
+        )}
+        <h1 className="title mt-5 is-3 is-size-4-mobile has-text-weight-semibold purple px-6">
+          Go to the <a className="gradient-text">organiser panel</a>
+        </h1>
+      </div>
+    ) : null
+  );
+};
+
+export default OrganisedEvents;
