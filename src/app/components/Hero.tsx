a1d0cb1f26017a7a34ce5c9cd4d75ae1dd288012
Anees Haroon <aharoon1007@gmail.com>
2023-08-17 11:22:40 +0100

    Format index Hero component code

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,19 +1,21 @@
-import Image from "next/image"
-import HeroImage from "../hero.png"
-import styles from './styles/Hero.module.css'
+import Image from "next/image";
+import HeroImage from "../hero.png";
+import styles from "./styles/Hero.module.css";
 
 const Hero = () => {
   return (
     <>
-        <div className={`${styles.hero} gradient`}>
-            <h1 className= {`${styles.title} has-text-white has-text-weight-semibold has-text-white mt-6 p-6`}>
-                Eventful Evenings <br></br>
-                Made Easy
-            </h1>
-            <Image src = {HeroImage} alt = "Hero" width={1000} height={1}/>
-        </div>
+      <div className={`${styles.hero} gradient`}>
+        <h1
+          className={`${styles.title} has-text-white has-text-weight-semibold has-text-white mt-6 p-6`}
+        >
+          Eventful Evenings <br></br>
+          Made Easy
+        </h1>
+        <Image src={HeroImage} alt="Hero" width={1000} height={1} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
