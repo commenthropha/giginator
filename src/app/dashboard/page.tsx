c0d44d95fe6b9aa3f246b2fed378da893e1905cd
Anees Haroon <aharoon1007@gmail.com>
2023-08-18 12:40:45 +0100

    Import OtherEvents component and add to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import {
   isUserOrganiser,
   getOtherEvents,
 } from "../(queries)";
-import {OrganisedEvents, UserEvents} from "./components";
+import { OrganisedEvents, UserEvents, OtherEvents } from "./components";
 
 const Dashboard = async () => {
   // Retrieve session data
@@ -35,43 +35,9 @@ const Dashboard = async () => {
   return (
     <div>
       <Header title="Dashboard" />
-      <OrganisedEvents events={organisedEvents} isOrganiser={isOrganiser}/>     
-      <UserEvents events = {userEvents} />
-
-      
-
-      {/* Upcoming Events that the user hasn't booked/isn't organising */}
-
-      <div id="organised-events" className="mb-6">
-        <h2 className="gradient-text title is-2 is-size-3-mobile has-text-weight-semibold m-6">
-          Upcoming Events
-        </h2>
-
-        {/* If there are no such events that fufill the requirements, 
-            let them know; otherwise return all the events that 
-            they have organised as card components*/}
-
-        {otherEvents === null ? (
-          <p className="mx-5 px-3 is-size-4 is-size-5-mobile">
-            <p className="mx-5 px-3 is-size-4 is-size-5-mobile">
-              {`There aren't any events that you haven't booked${
-                isOrganiser ? ` or that you aren't organising.` : "."
-              }`}
-            </p>
-          </p>
-        ) : (
-          <div className="is-desktop columns m-4">
-            {otherEvents.map((event: DBEvent) =>
-              event.tickets != event.capacity ? (
-                <Card key={event.id} event={event} text={true} />
-              ) : null
-            )}
-          </div>
-        )}
-        <h1 className="title mt-5 is-3 is-size-4-mobile has-text-weight-semibold purple px-6">
-          See all upcoming events <a className="gradient-text">here</a>
-        </h1>
-      </div>
+      <OrganisedEvents events={organisedEvents} isOrganiser={isOrganiser} />
+      <UserEvents events={userEvents} />
+      <OtherEvents events={otherEvents} isOrganiser={isOrganiser} />
     </div>
   );
 };
