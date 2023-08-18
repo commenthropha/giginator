540384802e22739bae1116d0f4655b498c856554
Anees Haroon <aharoon1007@gmail.com>
2023-08-18 18:52:18 +0100

    Add comments to EventCard.tsx

--- src/app/(global)/EventCard.tsx
+++ src/app/(global)/EventCard.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 
+// Create a type alias for the different card variants
 type CardType = "standard" | "booked" | "unavailable";
 
-type CardProps = {
+const EventCard = ({
+  cardType,
+  event,
+}: {
   cardType: CardType;
   event: DBEvent;
-}
-
-const EventCard = ({cardType, event}: CardProps) => {
+}) => {
+  // Evaluate the card type parameter
   switch (cardType) {
+    // For the "standard" event card variant
     case "standard":
       return (
         <div className="has-background-warning-light box mx-5 p-6">
@@ -25,9 +29,16 @@ const EventCard = ({cardType, event}: CardProps) => {
           <h2 className="mt-1 subtitle is-5 is-size-6-mobile">
             {event.duration}
           </h2>
-          <Link href = {`events/${event.id}`} className="has-text-weight-semibold mt-6 subtitle is-5 is-size-6-mobile">View This Event</Link>
+          <Link
+            href={`events/${event.id}`}
+            className="has-text-weight-semibold mt-6 subtitle is-5 is-size-6-mobile"
+          >
+            View This Event
+          </Link>
         </div>
       );
+
+    // For the "booked" event card variant
     case "booked":
       return (
         <div className="has-background-purple box mx-5 p-6">
@@ -47,9 +58,16 @@ const EventCard = ({cardType, event}: CardProps) => {
           <h2 className="has-text-white mt-1 subtitle is-5 is-size-6-mobile">
             {event.duration}
           </h2>
-          <Link href = {`events/${event.id}`}  className="mt-6 subtitle is-5 has-text-weight-semibold is-size-6-mobile has-text-grey-lighter">View This Event</Link>
+          <Link
+            href={`events/${event.id}`}
+            className="mt-6 subtitle is-5 has-text-weight-semibold is-size-6-mobile has-text-grey-lighter"
+          >
+            View This Event
+          </Link>
         </div>
       );
+
+    // For the "unavailable" event card variant
     case "unavailable":
       return (
         <div className="has-background-grey-light box mx-5 p-6">
@@ -66,7 +84,9 @@ const EventCard = ({cardType, event}: CardProps) => {
           <h2 className="mt-1 subtitle is-5 is-size-6-mobile">
             {event.duration}
           </h2>
-          <p className="mt-6 subtitle is-5 has-text-weight-semibold is-size-6-mobile">This Event Is Booked Out</p>
+          <p className="mt-6 subtitle is-5 has-text-weight-semibold is-size-6-mobile">
+            This Event Is Booked Out
+          </p>
         </div>
       );
   }
