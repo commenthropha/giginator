807b62bc842913c5732db56758e63cd1b0ffd9f3
Anees Haroon <aharoon1007@gmail.com>
2023-08-18 19:00:26 +0100

    Add comments to Header.tsx parameters

--- src/app/(global)/Header.tsx
+++ src/app/(global)/Header.tsx
@@ -1,4 +1,8 @@
-const Header = ({title}: {title: string}) => {
+const Header = ({
+  title // The text that we want the Header component to display
+}: {
+  title: string
+}) => {
   return (
     <div className="gradient">
       <h1 className="title is-1 is-size-2-mobile has-text-weight-semibold has-text-white p-12">
