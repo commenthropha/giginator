0878b1844e37c462c2142e36e43759f24b515444
Anees Haroon <aharoon1007@gmail.com>
2023-08-18 17:58:12 +0100

    Add sign in functionality for organiser test account

--- src/app/sign-in/components/OrganiserTest.tsx
+++ src/app/sign-in/components/OrganiserTest.tsx
@@ -1,11 +1,32 @@
 "use client";
-import {GrUserAdmin} from "react-icons/gr"
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { GrUserAdmin } from "react-icons/gr";
 
 const OrganiserTestButton = () => {
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithPassword({
+      email: "organiser@test",
+      password: "test",
+    });
+  };
+
   return (
-    <button className="button is-medium is-warning is-small-mobile is-flex p-3 is-justify-content-space-between" style={{width: "275px"}}>
+    <button
+      className="button is-medium is-warning is-small-mobile is-flex p-3 is-justify-content-space-between"
+      style={{ width: "275px" }}
+      onClick={async() => {
+        await handleSignIn();
+        router.push("/dashboard");
+        router.refresh();
+      }}
+    >
       <GrUserAdmin />
-      <p style={{fontSize: "1.125rem"}}>Organiser Test Account</p>
+      <p style={{ fontSize: "1.125rem" }}>Organiser Test Account</p>
     </button>
   );
 };
