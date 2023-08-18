074f1dd9367a3c89679f71046c91b45e4fb6ecf7
Anees Haroon <aharoon1007@gmail.com>
2023-08-18 14:09:29 +0100

    Add cancelTicket as a named export to the queries folder

--- src/app/(queries)/index.js
+++ src/app/(queries)/index.js
@@ -3,3 +3,5 @@ export {default as getUserEvents} from "./getUserEvents"
 export {default as getOrganisedEvents} from "./getOrganisedEvents"
 export {default as getOtherEvents} from "./getOtherEvents"
 export {default as isUserOrganiser} from "./isUserOrganiser"
+
+export {default as cancelTicket} from "./cancelTicket"
