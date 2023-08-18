4d881ce3f4169495c30dfbd903f72b3c62531e94
Anees Haroon <aharoon1007@gmail.com>
2023-08-18 22:33:53 +0100

    Add comments to events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,8 +2,11 @@ import { getSession } from "../(queries)";
 import { PublicPage, AuthenticatedPage } from "./components";
 
 const Events = async () => {
+  // Retrieve session data
   const session = await getSession();
 
+  // If logged in, return the authenticated page
+  // Otherwise, return the public page
   return session ? <AuthenticatedPage /> : <PublicPage />;
 };
 
