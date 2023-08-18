f8345f95bde7642ea6d1e12a5b1d3c9378dcd21f
Anees Haroon <aharoon1007@gmail.com>
2023-08-18 22:46:49 +0100

    Update Footer logo, text sizing

--- src/app/(global)/Footer.tsx
+++ src/app/(global)/Footer.tsx
@@ -5,11 +5,11 @@ const Footer = () => {
   return (
     <footer className="has-background-purple primary-dark p-5">
     <div className=" has-text-white content has-text-centered">
-      <p>
+      <p style={{fontSize: ".75rem"}}>
         <strong className = "has-text-white">The Giginator</strong> by Anees Haroon. The source code is licensed. The website content
         is licensed <a className ='has-text-white' href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
       </p>
-      <Image src={Logo} width="125" height="40" alt="Giginator Logo" />
+      <Image src={Logo} width="100" height="40" alt="Giginator Logo" />
     </div>
   </footer>
   )
