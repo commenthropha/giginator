5c9f6cb44c46162c6b97acfab2d25204b27ffd06
Anees Haroon <aharoon1007@gmail.com>
2023-08-19 20:54:27 +0100

    Add comments to sign in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,19 +1,21 @@
 import { redirect } from "next/navigation";
-import { getSession } from "../(queries)"
-import Form from "./components/Form"
+import { getSession } from "../(queries)";
+import Form from "./components/Form";
 
 const SignIn = async () => {
+  // Retrieve session data
   const session = await getSession();
 
+  // If not currently logged in
   if (session) {
     redirect("/dashboard");
   }
-  
+
   return (
     <>
-        <Form />
+      <Form />
     </>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
