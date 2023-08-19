659a4c96c627d82e519fe9ebed0c6bbc3a608296
Anees Haroon <aharoon1007@gmail.com>
2023-08-19 20:53:12 +0100

    Add comments to organisation page, OrganisedEvents component

--- src/app/organisation/components/OrganisedEvents.tsx
+++ src/app/organisation/components/OrganisedEvents.tsx
@@ -1,11 +1,15 @@
 import { EventCard } from "@/app/(global)";
 
 const OrganisedEvents = ({ events }: { events: DBEvent[] }) => {
+  // If there are no events
   return events.length == 0 ? (
     <p className="mx-6 px-1 is-size-5 is-size-6-mobile">
       {`You haven't organised any events yet.`}
     </p>
-  ) : (
+  ) 
+  
+  // Otherwise display the events as Event Cards
+  : (
     events.map((event: DBEvent) => (
       <EventCard key={event.id} event={event} cardType={"standard"} />
     ))
