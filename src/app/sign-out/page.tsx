ac2e07a1bb2a2b6c900176c0dc97abe296d04449
Anees Haroon <aharoon1007@gmail.com>
2023-08-19 20:55:16 +0100

    Add comments to sign out page

--- src/app/sign-out/page.tsx
+++ src/app/sign-out/page.tsx
@@ -1,19 +1,21 @@
 import { redirect } from "next/navigation";
-import { getSession } from "../(queries)"
-import Form from "./components/Form"
+import { getSession } from "../(queries)";
+import Form from "./components/Form";
 
 const SignOut = async () => {
+  // Retrieve session data
   const session = await getSession();
 
+  // If not currently logged in
   if (!session) {
     redirect("/");
   }
 
   return (
     <>
-        <Form />
+      <Form />
     </>
-  )
-}
+  );
+};
 
-export default SignOut
+export default SignOut;
