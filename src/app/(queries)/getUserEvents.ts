70be1c7b0ec476d12fc185bceb586dcd9b66ed23
Anees Haroon <aharoon1007@gmail.com>
2023-08-19 20:58:53 +0100

    Add comments to getUserEvents query

--- src/app/(queries)/getUserEvents.ts
+++ src/app/(queries)/getUserEvents.ts
@@ -1,29 +1,40 @@
-import { SupabaseClient, createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import {
+  SupabaseClient,
+  createServerComponentClient,
+} from "@supabase/auth-helpers-nextjs";
 import { getSession } from ".";
 import { cookies } from "next/headers";
 
 const getUserEvents = async () => {
+  // Initialise Supabase client
   const cookieStore = cookies();
-  const supabase: SupabaseClient = createServerComponentClient({ cookies: () => cookieStore });
+  const supabase: SupabaseClient = createServerComponentClient({
+    cookies: () => cookieStore,
+  });
+
+  // Retrieve session data
   const session = await getSession();
 
+  // Get the user event IDs from the association table
   const { data: userEvents } = await supabase
     .from("event_profiles")
     .select("event_id")
     .eq("profile_id", session?.user.id);
 
-    if (userEvents) {
-        const eventIds = userEvents.map((event) => event.event_id);
+  if (userEvents) {
+    // Extract the event IDs
+    const eventIds = userEvents.map((event) => event.event_id);
 
-        const { data: events } = await supabase
-          .from("events")
-          .select()
-          .in("id", eventIds);
-          
-        return events;
-    } else {
-        return null;
-    }
+    // Get the events associated with the IDs from the events table
+    const { data: events } = await supabase
+      .from("events")
+      .select()
+      .in("id", eventIds);
+
+    return events;
+  } else {
+    return null;
+  }
 };
 
 export default getUserEvents;
